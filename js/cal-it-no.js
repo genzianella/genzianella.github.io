e4659b1812f8c64d5c86a1f20236a58d9ef68298
Stefano <michste93@gmail.com>
2016-05-09 21:16:35 +0200

    second

--- js/cal-it-no.js
+++ js/cal-it-no.js
@@ -1,35 +1,34 @@
-jQuery(document).ready(function($){
-          
-	
-     $(function() {
-      
-$.datepicker.regional['it'] = {
-	                closeText: 'Chiudi',
-	                prevText: '&#x3c;Prec',
-	                nextText: 'Succ&#x3e;',
-	                currentText: 'Oggi',
-	                monthNames:['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
-	                monthNamesShort:['Gen','Feb','Mar','Apr','Mag','Giu','Lug','Ago','Set','Ott','Nov','Dic'],
-	                dayNames: ['Domenica','Luned&#236','Marted&#236','Mercoled&#236','Gioved&#236','Venerd&#236','Sabato'],
-	                dayNamesShort: ['Dom','Lun','Mar','Mer','Gio','Ven','Sab'],
-	                dayNamesMin: ['Do','Lu','Ma','Me','Gi','Ve','Sa'],
-	                weekHeader: 'Sm',
-	                dateFormat: 'dd/mm/yy',
-	                firstDay: 1,
-	                isRTL: false,
-	                showMonthAfterYear: false,
-	                yearSuffix: ''};
-	        $.datepicker.setDefaults($.datepicker.regional['it']);
-        
-        $("#depart").datepicker();
-        $("#return").datepicker();
-        });
-        
-  
-	
-  
- 
-	 
-	
-});
+if(typeof qFindContents=="undefined"){qFindContents=[]}qFindContents[qFindContents.length]={id:"QF1",content:'
+<form class="&#32;ui-not-initialized-widget" data-baseformurl="//web4.deskline.net/accommodationtrentino/it/accommodation/detail/TRN/685e206d-4fbd-41e2-8450-2bbcc0fdb286/x?dosearch=true" data-baselinkurl="//web4.deskline.net/accommodationtrentino/it/accommodation/detail/TRN/685e206d-4fbd-41e2-8450-2bbcc0fdb286/x" data-enhance="false" data-pagetype="AccommodationQFind" data-role="none" data-widget="dw_quickfind" data-width="180" id="QF1">
+    <div>
+        <input class="item&#32;ui-not-initialized-widget" data-enhance="false" data-role="none" data-visiblemonths="1" data-widget="dw_datepicker" data-width="178" id="QF1_arrival" name="QF1_arrival" placeholder="Arrivo"></input>
+        <input class="item&#32;ui-not-initialized-widget" data-enhance="false" data-role="none" data-visiblemonths="1" data-widget="dw_datepicker" data-width="178" id="QF1_departure" name="QF1_departure" placeholder="Partenza"></input>
+    </div>
+    <div class=\ 'bold padding2Bottom\' for=\ 'QF1_adults\'>Adulti</div>
+    <div class=\ 'display-table item width100\'>
+        <div class=\ 'cell valign-top\'>
+            <input class="&#32;ui-not-initialized-widget" data-contentid="QF1_adults_ddcontent" data-dropdownwidth="63" data-enhance="false" data-role="none" data-value="2" data-widget="dw_dropdown" data-width="63" id="QF1_adults" name="QF1_adults" readonly="readonly" type="text"></input>
+            <ul class="&#32;ui-not-initialized-widget" data-enhance="false" data-role="none" data-widget="dw_list" id="QF1_adults_ddcontent">
+                <li data-value="1">1 </li>
+                <li data-value="2">2 </li>
+                <li data-value="3">3 </li>
+                <li data-value="4">4 </li>
+                <li data-value="5">5 </li>
+                <li data-value="6">6 </li>
+                
+            </ul>
+        </div>
+        <div class=\ 'cell valign-top\'>
+            <button class="floatRight&#32;ui-not-initialized-widget" data-enhance="false" data-role="none" data-widget="dw_button" data-width="102" id="QF1_search" name="QF1_search" type="submit">Cerca</button>
+        </div>
+    </div>
+    <div class=\ 'bold item\'>Effettuare una ricerca dettagliata? Più unità, bambini ...</div>
+    <div class=\ 'bold item\'><a href=\ '#\' id=\ 'QF1_link\'>Vai alla ricerca dettagliata</a></div>
+    <div class="no-display" data-widget="dw_config">{"profile":{"GeneralSettingsNightsMaxNumber":30}}</div>
+</form>'}
 
+;if(typeof qFindSettings=="undefined"){
+qFindSettings={cssUrl:"//resc.deskline.net/DW/cs/ACCOWEB/DEFAULT/qfindstyles.css?63521566800",jqUrl:"//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js",jquiUrl:"//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js",localizationUrl:"//resc.deskline.net/DW/lk/accommodationtrentino/it/localization.it.js?v=118",qfindlibUrl:"//resc.deskline.net/DW/js/quickfindlib.js?v=118"};
+
+var script=document.createElement("script");script.type="text/javascript";
+script.src="//resc.deskline.net/DW/js/quickfind.js?v=118";document.getElementsByTagName("head")[0].appendChild(script)};
